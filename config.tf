abc7280a48f462d0d93b88e2172bde19ca5fdbdf
Narek Baghdasaryan <Narek_Baghdasaryan@epam.com>
2021-01-19 18:11:45 +0400

    Applied terraform fmt

--- config.tf
+++ config.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-tfstate-files-remote-s3-79686699"
-    key = "terraform_statefiles/terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "terraform-tfstate-files-remote-s3-79686699"
+    key            = "terraform_statefiles/terraform.tfstate"
+    region         = "us-east-1"
     dynamodb_table = "terraform-state-locking-ddb"
-    encrypt = true
+    encrypt        = true
   }
 }
